ed506e0559638839ea4d8e78256ef0ce7b8df035
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-04-04 18:45:22 +0530

    view issue page

--- src/Pages/Ec/Viewissue.js
+++ src/Pages/Ec/Viewissue.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import '../Ec css/Viewissue.css'
+import '../hod css/Exam.css'
+import '../hod css/Viewnotif.css'
 function Viewissue() {
   return (
-    <div>Viewissue</div>
+    <div>
+       <div className="title">View issue</div>
+       <div className="notification">
+         <p>Hod id :</p>
+         <p>Issue :</p>
+         <button className='logout replay-button'>Replay</button>
+       </div>
+    </div>
   )
 }
 
