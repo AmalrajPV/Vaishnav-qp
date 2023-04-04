a8c6332a98cd1cb59e1b85e79cf85d6cf2ae2f7a
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-04-04 18:54:45 +0530

    view issue page updated

--- src/Pages/Ec/Viewissue.js
+++ src/Pages/Ec/Viewissue.js
@@ -3,14 +3,18 @@ import '../Ec css/Viewissue.css'
 import '../hod css/Exam.css'
 import '../hod css/Viewnotif.css'
 function Viewissue() {
+  const issue = []
   return (
     <div>
        <div className="title">View issue</div>
+       {(issue.length >0) ? 
        <div className="notification">
          <p>Hod id :</p>
          <p>Issue :</p>
          <button className='logout replay-button'>Replay</button>
-       </div>
+       </div> :
+       <div className="exam-tile no-exam">No issues reported !!</div> }
+
     </div>
   )
 }
