cd9b0a39a10b31bdab4fb9cdf912d852a53f346e
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-04-26 00:48:48 +0530

    selection add , home page completed

--- src/Pages/Logins/Home.jsx
+++ src/Pages/Logins/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import img from '../../Assets/logo.png'
 import '../Logins/Login.css'
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-function Hod() {
+function Home() {
+    const options = [
+        // {value : 0 , label : "select "},
+        {value : 0 , label : "HOD"},
+        {value : 1 , label : "Exam Controller"},
+        {value : 2 , label : "Level Officer"}
+    ]
     return (
         <div className="main">
             <div className="nav">
@@ -27,7 +34,9 @@ function Hod() {
                     <form>
                         <div className="input-row">
                             <label htmlFor="hodid">User:</label>
-                            
+                            <div className="col-md-6">
+                                <Select options={options} className='selection_box'/>
+                            </div>
                         </div>
                         <div className="btn">
                             <input className='btn1' type="submit" value="Next" />
@@ -39,4 +48,4 @@ function Hod() {
     )
 }
 
-export default Hod
+export default Home
