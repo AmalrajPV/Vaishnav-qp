6a4c77a455e7002d746092d12cc52b99d6d565fe
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-04-26 22:34:36 +0530

    home to login route add

--- src/Pages/Logins/Home.jsx
+++ src/Pages/Logins/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img from '../../Assets/logo.png'
 import '../Logins/Login.css'
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+    const [optn,setOptn] = useState();
+    const navigate = useNavigate();
+    const handleClick =()=>{
+        switch (optn.value) {
+            case 0:
+                navigate("/hodlogin")
+                break;
+            case 1:
+                navigate("/eclogin")
+                break;
+            case 2:
+                navigate("/levellogin")
+                break;
+            default:
+                break;
+        }
+       
+    }
+
     const options = [
-        // {value : 0 , label : "select "},
         {value : 0 , label : "HOD"},
         {value : 1 , label : "Exam Controller"},
         {value : 2 , label : "Level Officer"}
@@ -35,17 +54,18 @@ function Home() {
                         <div className="input-row">
                             <label htmlFor="hodid">User:</label>
                             <div className="col-md-6">
-                                <Select options={options} className='selection_box'/>
+                                <Select options={options} onChange={setOptn}  className='selection_box'/>
                             </div>
                         </div>
                         <div className="btn">
-                            <input className='btn1' type="submit" value="Next" />
+                            <input className='btn1' type="submit" value="Next" onClick={handleClick} />
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     )
+    
 }
 
 export default Home
